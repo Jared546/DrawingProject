5b33fbc6ff8b10db710024fc971d3a41f297f882
Jared Diaz <Jared.Diaz.1890@gmail.com>
2016-10-10 19:14:29 -0700

    add some shapes (black color)

--- drawingproject.js
+++ drawingproject.js
@@ -1,6 +1,34 @@
 var canvasDiv = document.getElementById("drawingArea");
 var canvas = canvasDiv.getContext('2d');
 
-canvas.fillStyle = "red";
+canvas.fillStyle = "black";
 
-canvas.fillRect(0, 0, 100, 100);
+canvas.fillRect(600, 150, 160, 20);
+canvas.fillRect(580, 170, 20, 20);
+canvas.fillRect(560, 190, 20, 40);
+canvas.fillRect(760, 170, 20, 20);
+canvas.fillRect(780, 190, 20, 120);
+canvas.fillRect(640, 250, 180, 20);
+canvas.fillRect(540, 250, 180, 20);
+canvas.fillRect(520, 230, 20, 20);
+canvas.fillRect(540, 210, 20, 20);
+canvas.fillRect(600, 270, 20, 40);
+canvas.fillRect(660, 270, 20, 40);
+canvas.fillRect(680, 270, 100, 20);
+canvas.fillRect(760, 230, 20, 20);
+canvas.fillRect(560, 270, 20, 80);
+canvas.fillRect(580, 350, 20, 20);
+canvas.fillRect(740, 350, 40, 20);
+canvas.fillRect(600, 370, 180, 20);
+canvas.fillRect(780, 310, 20, 40);
+canvas.fillRect(820, 480, 20, 20);
+
+canvas.fillStyle = "red";
+canvas.fillRect(600, 170, 160, 20);
+canvas.fillRect(580, 190, 200, 20);
+canvas.fillRect(600, 210, 180, 20);
+canvas.fillRect(620, 230, 140, 20);
+canvas.fillRect(580, 250, 60, 20);
+
+canvas.fillStyle = "lightblue";
+canvas.fillRect(780, 350, 20, 40);
